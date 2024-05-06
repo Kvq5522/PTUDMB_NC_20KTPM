756eeafa70c3da9f10788e16511e515774e61d48
HQDuy47 <72744215+HQDuy47@users.noreply.github.com>
2024-05-06 16:35:17 +0700

    fix conflicts 2

--- lib/screens/messages/widget/schedule.dart
+++ lib/screens/messages/widget/schedule.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:studenthub/screens/messages/widget/schedule_item.dart';
